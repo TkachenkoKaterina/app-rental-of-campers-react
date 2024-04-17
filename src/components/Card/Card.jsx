03a0e4815709c2c12e83489debacc9257442f6ae
Katerina <90516132+TkachenkoKaterina@users.noreply.github.com>
2024-04-17 21:43:07 +0300

    markup Card Component without css, made sprite.svg

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,87 @@
 import React from "react";
+import Button from "../Button/Button";
+
+import sprite from "../../utils/svg/sprite.svg";
 
 import css from "./Card.module.css";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className={css.cardWrapper}>
+      <img src="" alt="" />
+      <div className={css.cardInfo}>
+        <div className={css.cardTitle}>
+          <h2 className={css.title}>Mavericks</h2>
+          <div className={css.cardPrice}>
+            <p className={css.price}>€8000.00</p>
+            <svg width="24" height="24">
+              <use xlinkHref={`${sprite}#heart_default`} />
+            </svg>
+          </div>
+        </div>
+
+        <div className={css.cardReviews}>
+          <div className={css.review}>
+            <svg width="24" height="24">
+              <use xlinkHref={`${sprite}#heart_pressed`} />
+            </svg>
+            <p>4.4(2 Reviews)</p>
+          </div>
+
+          <div className={css.location}>
+            <svg>
+              <use />
+            </svg>
+            <p>Kyiv, Ukraine</p>
+          </div>
+        </div>
+        <p>
+          Embrace simplicity and freedom with the Mavericks panel truck, an
+          ideal choice for solo travelers or couples seeking a compact and
+          efficient way to explore the open roads.
+        </p>
+        <ul>
+          <li className={css.item}>
+            <svg>
+              <use />
+            </svg>
+            <p>adults</p>
+          </li>
+          <li className={css.item}>
+            <svg>
+              <use />
+            </svg>
+            <p>adults</p>
+          </li>
+          <li className={css.item}>
+            <svg>
+              <use />
+            </svg>
+            <p>adults</p>
+          </li>
+          <li className={css.item}>
+            <svg>
+              <use />
+            </svg>
+            <p>adults</p>
+          </li>
+          <li className={css.item}>
+            <svg>
+              <use />
+            </svg>
+            <p>adults</p>
+          </li>
+          <li className={css.item}>
+            <svg>
+              <use />
+            </svg>
+            <p>adults</p>
+          </li>
+        </ul>
+        <Button />
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Card from "../../components/Card/Card";
 import FormSearch from "../../components/FormSearch/FormSearch";
+
+import css from "./Catalog.module.css";
 
 const Catalog = () => {
   return (
-    <div>
+    <div className={css.wrapper}>
       <FormSearch />
+      <Card />
     </div>
   );
 };
