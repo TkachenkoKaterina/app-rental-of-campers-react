1ac8c2a0977d4ce59a7b21e653460e613a4e3c03
Katerina <90516132+TkachenkoKaterina@users.noreply.github.com>
2024-04-23 22:45:26 +0300

    markup card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,81 +4,92 @@ import Button from "../Button/Button";
 import sprite from "../../utils/svg/sprite.svg";
 
 import css from "./Card.module.css";
+import IMG from "../../utils/img/switch.jpg";
 
 const Card = () => {
   return (
     <div className={css.cardWrapper}>
-      <img src="" alt="" />
+      <div>
+        <img src={IMG} alt="camper" width="290" height="310" />
+      </div>
       <div className={css.cardInfo}>
-        <div className={css.cardTitle}>
-          <h2 className={css.title}>Mavericks</h2>
-          <div className={css.cardPrice}>
-            <p className={css.price}>€8000.00</p>
-            <svg width="24" height="24">
-              <use xlinkHref={`${sprite}#heart_default`} />
-            </svg>
-          </div>
-        </div>
-
-        <div className={css.cardReviews}>
-          <div className={css.review}>
-            <svg width="24" height="24">
-              <use xlinkHref={`${sprite}#heart_pressed`} />
-            </svg>
-            <p>4.4(2 Reviews)</p>
+        <div className={css.cardInfoReview}>
+          <div className={css.cardTitle}>
+            <h2 className={css.title}>Mavericks</h2>
+            <div className={css.cardPrice}>
+              <p className={css.title}>€8000.00</p>
+              <svg className={css.heart} width="24" height="24">
+                <use xlinkHref={`${sprite}#heart`} />
+              </svg>
+            </div>
           </div>
 
-          <div className={css.location}>
-            <svg>
-              <use />
-            </svg>
-            <p>Kyiv, Ukraine</p>
+          <div className={css.cardReviews}>
+            <div className={css.review}>
+              <svg width="16" height="16">
+                <use xlinkHref={`${sprite}#raiting`} />
+              </svg>
+              <p className={css.reviewRating}>4.4(2 Reviews)</p>
+            </div>
+
+            <div className={css.review}>
+              <svg width="16" height="16">
+                <use xlinkHref={`${sprite}#map-pin`} />
+              </svg>
+              <p>Kyiv, Ukraine</p>
+            </div>
           </div>
         </div>
-        <p>
-          Embrace simplicity and freedom with the Mavericks panel truck, an
-          ideal choice for solo travelers or couples seeking a compact and
-          efficient way to explore the open roads.
+        <p className={css.reviewText}>
+          Embrace simplicity and freedom with the Mavericks panel truck, an...
         </p>
-        <ul>
-          <li className={css.item}>
-            <svg>
-              <use />
+        <ul className={css.categoryList}>
+          <li className={css.categoryItems}>
+            <svg width="20" height="20">
+              <use xlinkHref={`${sprite}#users`} />
+            </svg>
+            <p className={css.categoryItemsText}>adults</p>
+          </li>
+          <li className={css.categoryItems}>
+            <svg width="20" height="20">
+              <use xlinkHref={`${sprite}#users`} />
             </svg>
             <p>adults</p>
           </li>
-          <li className={css.item}>
-            <svg>
-              <use />
+          <li className={css.categoryItems}>
+            <svg width="20" height="20">
+              <use xlinkHref={`${sprite}#users`} />
             </svg>
             <p>adults</p>
           </li>
-          <li className={css.item}>
-            <svg>
-              <use />
+          <li className={css.categoryItems}>
+            <svg width="20" height="20">
+              <use xlinkHref={`${sprite}#users`} />
             </svg>
             <p>adults</p>
           </li>
-          <li className={css.item}>
-            <svg>
-              <use />
+          <li className={css.categoryItems}>
+            <svg width="20" height="20">
+              <use xlinkHref={`${sprite}#users`} />
             </svg>
             <p>adults</p>
           </li>
-          <li className={css.item}>
-            <svg>
-              <use />
+          <li className={css.categoryItems}>
+            <svg width="20" height="20">
+              <use xlinkHref={`${sprite}#users`} />
             </svg>
             <p>adults</p>
           </li>
-          <li className={css.item}>
-            <svg>
-              <use />
+          <li className={css.categoryItems}>
+            <svg width="20" height="20">
+              <use xlinkHref={`${sprite}#users`} />
             </svg>
             <p>adults</p>
           </li>
         </ul>
-        <Button />
+        <div>
+          <Button />
+        </div>
       </div>
     </div>
   );
